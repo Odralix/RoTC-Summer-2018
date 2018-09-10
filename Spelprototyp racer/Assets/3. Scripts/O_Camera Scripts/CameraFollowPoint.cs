19128dd7a864db5e4211b41e0bae49f86a084d5c
Odralix <ossian.edstrom@live.se>
2018-09-10 17:08:21 +0200

    Added functionalities to Main Menu

---
+++ Spelprototyp racer/Assets/3. Scripts/O_Camera Scripts/CameraFollowPoint.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollowPoint : MonoBehaviour {
+
+    private new Vector3 origPos;
+    public new Vector3 newPos;
+    public new Vector3 inBetweenPos1;
+    public new Vector3 inBetweenPos2;
+    public float time2;
+    private bool hasSwitched = false;
+	// Use this for initialization
+	void Start ()
+    {
+		origPos = transform.position;
+	}
+	
+    public void CameraSwitch()
+    {
+        if(hasSwitched == false)
+        {
+            transform.position = inBetweenPos1;
+            StartCoroutine(Wait(0.1f));
+            //transform.position = newPos;
+            //hasSwitched = true;
+        }
+        else if(hasSwitched == true)
+        {
+            transform.position = inBetweenPos2;
+            StartCoroutine(Wait(0.1f));
+            //transform.position = origPos;
+            //hasSwitched = false;
+        }
+
+        //DelayMenu(time2);
+
+    }
+
+    private IEnumerator Wait(float time)
+    {
+        yield return new WaitForSeconds(time);
+        if (hasSwitched == false)
+        {
+            transform.position = newPos;
+            hasSwitched = true;
+        }
+        else if(hasSwitched == true)
+        {
+            transform.position = origPos;
+            hasSwitched = false;
+        }
+    }
+
+    //private IEnumerator DelayMenu(float time)
+    //{
+    //    yield return new WaitForSeconds(time);
+    //    int five = 5;
+    //    five = 1 + 3;
+    //}
+}
---
+++ Spelprototyp racer/Assets/3. Scripts/O_Camera Scripts/LowerScript.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LowerScript : MonoBehaviour
+{
+
+    public float howLow;
+    public Animator theAnimator;
+    private float howHigh;
+    // Use this for initialization
+    void Start()
+    {
+        howHigh = transform.position.y;
+    }
+
+    public void Lower()
+    {
+        theAnimator.SetBool("SignDown", true);
+        //Vector3 tmp = transform.position;
+        //tmp.y = howLow;
+        //transform.position = tmp;
+        //transform.position.Set(transform.position.x, howLow, transform.position.z);
+    }
+    public void Raise()
+    {
+        theAnimator.SetBool("SignDown", false);
+        //Vector3 tmp = transform.position;
+        //tmp.y = howHigh;
+        //transform.position = tmp;
+        //transform.position.Set(transform.position.x, howHigh, transform.position.z);
+    }
+}
